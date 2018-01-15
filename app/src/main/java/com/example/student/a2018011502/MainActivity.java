2855389902c0dbc124beda35f8bdcb1b45e24589
miowoo <miowoo821@gmail.com>
2018-01-15 12:17:12 +0800

    準備做AsyncTask(可取代Thread的東西)

--- app/src/main/java/com/example/student/a2018011502/MainActivity.java
+++ app/src/main/java/com/example/student/a2018011502/MainActivity.java
@@ -2,10 +2,13 @@ package com.example.student.a2018011502;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import java.io.BufferedInputStream;
 import java.io.ByteArrayOutputStream;
@@ -17,11 +20,15 @@ import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
     ImageView img;
+    TextView tv;
+    ProgressBar pb;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         img=(ImageView)findViewById(R.id.imageView);
+        tv=(TextView)findViewById(R.id.textView);
+        pb=(ProgressBar)findViewById(R.id.progressBar);
     }
     public void click1(View v){
 
@@ -39,10 +46,28 @@ public class MainActivity extends AppCompatActivity {
                     conn.connect();
                     InputStream inputStream=conn.getInputStream();
                     ByteArrayOutputStream bos=new ByteArrayOutputStream();
-                    byte[] buf=new byte[1024];
-                    int length;
-                    while ((length=inputStream.read(buf))!=-1){
+                    byte[] buf=new byte[1024];//做一個位元組陣列，大小1024(要幹嘛?)
+
+                    final int totallength=conn.getContentLength();//把conn的總大小放進名為totallength的int型態的變數中
+                    int sum=0;//新增一個名為sum的int變數
+
+                    int length;//新增一個名為length的int變數
+                    while ((length=inputStream.read(buf))!=-1){//當把每次從buf讀到的東西丟進length中，並判斷length會不會等於-1(沒有資料的時候讀到的就是-1，有資料的時候讀到的是buf的1024陣列)
+                       // sum += length;//sum=sum+length的意思
+                        sum=sum+length;
+
+                        final int tmp=sum;//把sum變數丟進tmp中
+
                         bos.write(buf,0,length);
+
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                tv.setText(String.valueOf(tmp));
+                                pb.setProgress(100 * tmp / totallength);
+                                img.setVisibility(View.VISIBLE);
+                            }
+                        });
                     }
 
                     byte[] results=bos.toByteArray();
@@ -66,4 +91,14 @@ public class MainActivity extends AppCompatActivity {
         }.start();//不要忘了開始
 
     }
+
+    //AsyncTask<>泛型，傳入值，回報值，結果值
+    class MyTask extends AsyncTask<Integer,Integer,String> {
+
+        @Override
+        protected String doInBackground(Integer... integers) {
+            return null;
+        }
+    }
+
 }
